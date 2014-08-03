127745b90e556b7b6965bb812fc54d5117ba57aa
Tor Henning Ueland <tor.henning@gmail.com>
2014-08-03 03:08:27 +0200

    fjernet multithreading fra java, dokumentet i run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,7 @@
 checksToDo=100000
 ant
 java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
-#java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo &
+
+# en veldig fin løsning, samle opp alle kommandoene som skal kjøres i en egen fil, en kommando per linje.
+# den catter du inn i xargs --max-proces=<antall CPUer>, da vil xargs fordele alt utover så det alltid kjører
+# N tråder.   Tiperdet mellom hver kjøring må tas en kopi av .minecraft mappa, og gamedir sendes med som java-opsjon
