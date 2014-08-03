e9111c0d1e66e443253a8bdfd2cf34370e533b45
Tor Henning Ueland <tor.henning@gmail.com>
2014-08-03 17:47:55 +0200

    forking i run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-checksToDo=100000
+biomes="Plains,Mesa,Roofed Forest,Taiga,Jungle,Savanna"
+checksToDo=10000000
+startSeed=912850000
+checksPerJob=5000000
+mcDir=~/.minecraft
+tmpDir=/tmp/mc
 ant
-java -jar dist/findbiome.jar `expr $RANDOM \* 99999999` $checksToDo
-
+mkdir $tmpDir
+for ((i=$checksToDo;i>=0;i-=$checksPerJob));
+do
+	j=$(($j+$startSeed))
+	mcTmp="$tmpDir/$RANDOM"
+	rsync -r $mcDir $mcTmp
+	echo "Rsyncing data to $mcTmp"
+	echo "java -Dbiomefinder.mcpath=$mcTmp -Dbiomefinder.debug=false -Dbiomefinder.biomes='Taiga,Plains,Mesa,Jungle,Desert,Roofed Forest,Forest,Savanna' -jar dist/findbiome.jar $j $checksPerJob" >> $tmpDir/cmd.args
+done
+procs=`cat /proc/cpuinfo|grep processor -c`
+xargs --max-procs=$procs --arg-file=$tmpDir/cmd.args > result.txt
+rm -rf $tmpDir
 # en veldig fin løsning, samle opp alle kommandoene som skal kjøres i en egen fil, en kommando per linje.
 # den catter du inn i xargs --max-proces=<antall CPUer>, da vil xargs fordele alt utover så det alltid kjører
 # N tråder.   Tiperdet mellom hver kjøring må tas en kopi av .minecraft mappa, og gamedir sendes med som java-opsjon
