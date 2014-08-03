ad075860f12ed1da9ddbca53e7714f1bcd0b5869
Tor Henning Ueland <tor.henning@gmail.com>
2014-08-03 19:16:20 +0200

    Got paralellism up and going as we want i

--- run.sh
+++ run.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+#
+# Script for running BiomeFinder in paralell
+# Author: Tor Henning Ueland < tor.henning@mail.com >
+#
+#config start
 BIOMES="Plains,Mesa,Roofed Forest,Taiga,Jungle,Savanna"
-TOCHECK=15000000
-STARTSEED=0
-SEEDSPERJOB=4999999
+TOCHECK=10000000
+STARTSEED=20000000
+SEEDSPERJOB=1000000
 BASEDIR=~/.minecraft
 TMPDIR=/tmp
+#config end 
 ant
 CURRENTSTARTSEED=$STARTSEED
 for ((i=$TOCHECK;i>=0;i-=$SEEDSPERJOB));
 do
 	CURRENTTEMP=$(mktemp -d ${TMPDIR}/mctmp.XXXXXXXX)
-	rsync -r $BASEDIR $CURRENTTEMP
+	rsync -r "$BASEDIR/" "$CURRENTTEMP/."
 	echo "Rsyncing data to $CURRENTTEMP"
 	CURRENTENDSEED=$(($CURRENTSTARTSEED+$SEEDSPERJOB))
-	echo -n "echo java -Dbiomefinder.mcpath=$CURRENTTEMP -Dbiomefinder.biomes='${BIOMES}' -jar dist/findbiome.jar " >> ${TMPDIR}/cmd.args
-	echo "${CURRENTSTARTSEED} ${SEEDSPERJOB} >> $tmpDir/scan-${CURRENTSTARTSEED}-to-${CURRENTENDSEED}.log" >> ${TMPDIR}/cmd.args
+	echo "java -Dbiomefinder.mcpath=$CURRENTTEMP -Dbiomefinder.biomes=\\\"${BIOMES}\\\" -jar dist/findbiome.jar ${CURRENTSTARTSEED} ${SEEDSPERJOB};" >> ${TMPDIR}/cmd.args
 	CURRENTSTARTSEED=$(($CURRENTENDSEED+1))
 done
 procs=`cat /proc/cpuinfo|grep processor -c`
-#xargs --max-procs=$procs --arg-file=$tmpDir/cmd.args > result.txt
-cat ${TMPDIR}/cmd.args
+cat ${TMPDIR}/cmd.args|xargs --max-procs=$procs  -I CMD bash -c CMD > result.txt
 rm -rf ${TMPDIR}/mctmp.*
 rm ${TMPDIR}/cmd.args
